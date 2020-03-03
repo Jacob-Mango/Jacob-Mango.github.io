78d262119a60d43ac395a32367f3ab0902ed4360
Jacob_Mango <mangosprograms@gmail.com>
2020-03-03 17:23:46 +1100

    using reactjs now

---
+++ precache-manifest.ea8faa14b216df24e002180320d1f823.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0157d834aee8a63db550f03615c88b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48f0c640f7c9e4e3425a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "61ba05e89649e4cd4054",
+    "url": "/static/css/main.c68df08c.chunk.css"
+  },
+  {
+    "revision": "48f0c640f7c9e4e3425a",
+    "url": "/static/js/2.e69432dd.chunk.js"
+  },
+  {
+    "revision": "a02c2e6156cb6e9e5c30be88d1ba3366",
+    "url": "/static/js/2.e69432dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ba05e89649e4cd4054",
+    "url": "/static/js/main.ed5cf646.chunk.js"
+  },
+  {
+    "revision": "9770eeae018faa6cfa76",
+    "url": "/static/js/runtime-main.24ae2bf3.js"
+  }
+]);
